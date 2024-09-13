aa7dbb8f127d9198a8aedb8659816526a55c79df
Timothy Lim <contacttimothylim@gmail.com>
2024-09-13 10:11:11 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -59,6 +59,45 @@ function handlePieceClick(event) {
     highlightPossibleMoves();
 }
 
+function highlightPossibleMoves() {
+    // Clear previous highlights
+    gameState.possibleMoves.forEach(move => {
+        const cell = document.querySelector(`[data-row="${move.row}][data-col="${move.col}]`);
+        cell.classList.remove('highlight');
+    });
+    gameState.possibleMoves = calculatePossibleMovies(gameState.selectedPiece);
+    gameState.possibleMoves.forEach(move => {
+        const cell = document.querySelector(`[data-row="${move.row}"][data-col="${move.col}"]`);
+        cell.classList.add('highlight');
+        cell.addEventListener('click', handleMove);
+    });
+}
+
+function calculatePossibleMoves(piece) {
+    const possibleMoves = [];
+    const directions = gameState.currentPlayer === 'P1' ? [[-1, 1], [-1, 1]] : [[1, -1], [1, 1]];
+    directions.forEach(direction => {
+        const newRow = piece.row + direction[0];
+        const newCol = piece.col + direction [1];
+        if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8 && gameState.board[newRow][newCol] === null) {
+            possibleMoves.push({ row: newRow, col: newCol});
+        }
+    });
+    return possibleMoves;
+}
+
+function handleMove(event) {
+    const cell = event.target;
+    const newRow = parseInt(cell.dataset.row);
+    const newCol = parseInt(cell.dataset.col);
+    gameState.board[newRow][newCol] = gameState.currentPlayer;
+    gameState.board[gameState.selectedPiece.row][gameState.selectedPiece.col] = null;
+    gameState.selectedPiece = null;
+    gameState.possibleMoves = [];
+    renderBoard();
+    switchPlayer();
+}
+
 function updatePlayerTurn() {
     const playerTurnE = document.getElementById('player-turn');
     playerTurnE.innerHTML = `<b> Current Player: ${gameState.currentPlayer}</b>`;
@@ -73,8 +112,4 @@ function switchPlayer() {
 document.addEventListener('DOMContentLoaded', () => {
     renderBoard();
     updatePlayerTurn();
-});
-
-document.querySelectorAll('.piece').forEach(piece => {
-    piece.addEventListener('click', handlePieceClick); 
 });
